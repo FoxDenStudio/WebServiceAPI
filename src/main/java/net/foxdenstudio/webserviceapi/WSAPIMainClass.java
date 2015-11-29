041be0d190c497fc2a272b486941d628512b2785
Joshua Freedman <npjoshf@gmail.com>
2015-11-29 02:41:24 -0800

    Starting setup...

---
+++ src/main/java/net/foxdenstudio/webserviceapi/WSAPIMainClass.java
@@ -0,0 +1,12 @@
+package net.foxdenstudio.webserviceapi;
+
+import org.spongepowered.api.plugin.Plugin;
+
+/**
+ * Created by Joshua Freedman on 11/29/2015.
+ * Project: SpongeForge->FDS-WSAPI
+ */
+@Plugin(name = "FoxDenStudio - WSAPI", id = "ffds-wsapi")
+public class WSAPIMainClass {
+
+}
