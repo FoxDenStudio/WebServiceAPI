af81aa2298d5d0bbfef9691ceed4c6fb71a2e550
Joshua Freedman <npjoshf@gmail.com>
2015-11-29 03:31:53 -0800

    Testing things...

--- src/main/java/net/foxdenstudio/webserviceapi/WSAPIMainClass.java
+++ src/main/java/net/foxdenstudio/webserviceapi/WSAPIMainClass.java
@@ -2,11 +2,14 @@ package net.foxdenstudio.webserviceapi;
 
 import com.google.inject.Inject;
 import net.foxdenstudio.webserviceapi.annotations.RequestHandler;
+import net.foxdenstudio.webserviceapi.requests.IWebServiceRequest;
+import net.foxdenstudio.webserviceapi.responses.IWebServiceResponse;
 import org.slf4j.Logger;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.plugin.Plugin;
 import org.spongepowered.api.plugin.PluginManager;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -49,5 +52,19 @@ public class WSAPIMainClass {
         System.out.println("TEST:" + pluginAndPathRegistry);
     }
 
+    public void callTest(String path, String path2, IWebServiceRequest serviceRequest) {
+        if (pluginAndPathRegistry.containsKey(path)) {
+            HashMap<String, RequestHandlerData> dataHashMap = pluginAndPathRegistry.get(path);
+            if (dataHashMap.containsKey(path2)) {
+                RequestHandlerData requestHandlerData = dataHashMap.get(path2);
+                try {
+                    System.out.println("ATTEMPT: " + path + " / " + path2);
+                    requestHandlerData.getMethod().invoke(requestHandlerData.getClassInstance(), serviceRequest);
+                } catch (IllegalAccessException | InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
